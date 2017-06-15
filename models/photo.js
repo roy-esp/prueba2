d588176eb3a2d4a5dff65fa27b5dc02be6deca5f
roy-esp <adrianroy1@hotmail.com>
2017-06-15 19:06:25 +0200

    prueba
    asdasd

---
+++ models/photo.js
@@ -0,0 +1,15 @@
+/* 
+
+Modelo de datos de canciones (photo)
+
+photo_id: {
+	name: nombre de la canción,
+	url: url del fichero de audio
+} 
+
+*/
+exports.photos = {
+	
+}; 
+
+var photos_url = process.env.PHOTOS_URL || "http://10.1.3.1:8000"
